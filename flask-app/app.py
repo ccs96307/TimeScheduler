9b95f5ff42d10c6be9ec613c6ae1d4faafd621e0
ccs96307 <ccs96307@gmail.com>
2023-05-08 16:47:37 +0800

    [Rename] folder

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,8 +5,8 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(
     __name__, 
-    static_folder="../my-app/build", 
-    template_folder="../my-app/build",
+    static_folder="../react-app/build", 
+    template_folder="../react-app/build",
 )
 
 @app.route("/", defaults={"path": ""})
